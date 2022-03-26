fad2b0c7f218b97bdffab07c2a92dbb7237ad900
GeniusCorn <hi@nicecorn.com>
2022-03-26 11:47:04 +0800

    feat: 星级展示组件

---
+++ src/components/StarRating.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+    score: number;
+}>();
+</script>
+
+<template>
+    <span v-for="n in props.score" :key="n" class="mx-2">★</span>
+</template>
+
+<style scoped></style>
--- src/models/shop.ts
+++ src/models/shop.ts
@@ -5,16 +5,18 @@ import image1 from '../assets/images/mae-mu-t-0rX27Xiy0-unsplash.jpg';
 export default interface Shop {
     logoUrl: string;
     name: string;
+    score: number;
     shipper: string;
     diversityTime: string;
-    descripton: string;
+    description: string;
 }
 
 export const shop: Shop = reactive({
     logoUrl: image1,
     name: '城里粥坊',
+    score: 4,
     shipper: '蜂鸟专送',
     diversityTime: '30',
-    descripton:
-        '粥在我国至少有五千年的历史了，可谓源远流长。自古就有将粥喻作“滋生育神丹”、“滋养胃气妙品”、“世间第一补人之物”。城里粥坊其烹饪粥料地秘方源于中国千年古法，再融合现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0 添加地良心品质深受消费者青睐，发展至今成为粥类地引领品牌。',
+    description:
+        '粥在我国至少有五千年的历史了，可谓源远流长。自古就有将粥喻作“滋生育神丹”、“滋养胃气妙品”、“世间第一补人之物”。城里粥坊其烹饪粥料地秘方源于中国千年古法，再融合现代制作工艺，由世界烹饪大师屈浩先生领衔研发。坚守纯天然、0 添加的良心品质深受消费者青睐，发展至今成为粥类地引领品牌。',
 });
