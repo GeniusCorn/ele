258bcd5b56218ef7976682ab0758bbe02e2ca6ed
GeniusCorn <hi@nicecorn.com>
2022-03-27 20:08:17 +0800

    feat: 商品组件

---
+++ src/components/MainView/CommodityItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import CountItem from './CountItem.vue';
+
+import { commodity } from '../../models/commodity';
+
+const props = defineProps<commodity>();
+</script>
+
+<template>
+    <div class="flex flex-row border-b-2 px-1 py-4">
+        <img
+            :src="props.img"
+            :alt="props.name"
+            class="object-cover w-20 h-20"
+        />
+        <div class="px-2">
+            <div class="text-lg">
+                {{ props.name }}
+            </div>
+            <span class="text-sm text-red-400">
+                月售 {{ props.monthly_sale }}
+            </span>
+            <span class="text-sm text-red-400"> 好评 {{ props.praise }} </span>
+            <div class="text-sm">￥{{ props.price }}</div>
+        </div>
+        <div class="flex flex-col-reverse">
+            <CountItem></CountItem>
+        </div>
+    </div>
+</template>
---
+++ src/components/MainView/CountItem.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const count = ref<number>(0);
+
+const addCount = () => {
+    count.value += 1;
+};
+
+const reduceCount = () => {
+    count.value -= 1;
+};
+
+const isInvisible = ref(true);
+watch(count, () => {
+    if (count.value > 0) {
+        isInvisible.value = false;
+    } else {
+        isInvisible.value = true;
+    }
+});
+</script>
+
+<template>
+    <div class="flex flex-row items-center">
+        <span
+            :class="{ invisible: isInvisible }"
+            class="btn outline-blue-400 bg-blue-400"
+            @click="reduceCount"
+        >
+            -
+        </span>
+
+        <span :class="{ invisible: isInvisible }" class="w-8 text-center">
+            {{ count }}
+        </span>
+
+        <span class="btn outline-red-400 bg-red-400" @click="addCount">
+            +
+        </span>
+    </div>
+</template>
+
+<style scoped>
+.btn {
+    @apply rounded-full outline outline-2 w-4 h-4 flex items-center justify-center;
+}
+</style>
