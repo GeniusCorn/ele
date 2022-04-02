562165d0c6e5c0f4e124175072d5a294eddc9bc5
GeniusCorn <hi@nicecorn.com>
2022-04-02 17:55:31 +0800

    feat: 星际组件增加空星的显示

--- src/components/common/StarRating.vue
+++ src/components/common/StarRating.vue
@@ -1,21 +1,31 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import StarRound from '@vicons/material/StarRound';
 import StarHalfRound from '@vicons/material/StarHalfRound';
+import StarBorderRound from '@vicons/material/StarBorderRound';
 
 const props = defineProps<{
     score: number;
 }>();
 
-const total = Math.trunc(props.score);
-const half = props.score - total;
+const totalStars = Math.trunc(props.score);
+const halfStar = props.score - totalStars;
+let borderStars;
+if (halfStar >= 0.5) {
+    borderStars = Math.trunc(5 - props.score);
+} else {
+    borderStars = Math.ceil(5 - props.score);
+}
 </script>
 
 <template>
     <div class="flex flex-row">
-        <div v-for="n in total" :key="n">
+        <div v-for="n in totalStars" :key="n">
             <StarRound class="h-4"></StarRound>
         </div>
-        <StarHalfRound v-if="half >= 0.5" class="h-4"></StarHalfRound>
+        <StarHalfRound v-if="halfStar >= 0.5" class="h-4"></StarHalfRound>
+        <div v-for="n in borderStars" :key="n">
+            <StarBorderRound class="h-4"></StarBorderRound>
+        </div>
     </div>
 </template>
 
