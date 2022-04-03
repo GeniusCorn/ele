5eba9a92fdf62d7067adf8c5fbb0cb84e8757ae5
GeniusCorn <hi@nicecorn.com>
2022-04-03 11:22:35 +0800

    feat: 评论内容组件

---
+++ src/components/comment/CommentContent.vue
@@ -0,0 +1,79 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { Icon } from '@vicons/utils';
+import PeopleAltFilled from '@vicons/material/PeopleAltFilled';
+import ThumbUpOffAltRound from '@vicons/material/ThumbUpOffAltRound';
+
+import StarRating from '../common/StarRating.vue';
+
+const props = defineProps<{
+    avator: string;
+    name: string;
+    rating: number;
+    comment: string;
+    tag: string[];
+    time: string;
+}>();
+
+const name = computed(() => {
+    const firstChar = props.name.charAt(0);
+    const lastChar = props.name.charAt(props.name.length - 1);
+    let char = firstChar;
+    if (props.name.length === 2) {
+        char += '*';
+    } else {
+        for (let i = 0; i < props.name.length - 2; i += 1) {
+            char += '*';
+        }
+    }
+
+    char += lastChar;
+    return char;
+});
+</script>
+
+<template>
+    <div class="flex flex-row ml-6 border-b-2 pb-4 m-8">
+        <div>
+            <Icon color="rgb(96, 165, 250)" size="42">
+                <PeopleAltFilled
+                    class="border-2 rounded-full"
+                ></PeopleAltFilled>
+            </Icon>
+        </div>
+        <div class="ml-4">
+            <div class="grid grid-cols-2 w-fit">
+                <div>
+                    {{ name }}
+                </div>
+                <div>
+                    {{ props.time }}
+                </div>
+            </div>
+            <div class="flex flex-row items-center">
+                <StarRating :score="props.rating"></StarRating>
+                <div class="ml-2">
+                    {{ props.rating }}
+                </div>
+            </div>
+            <br />
+            <div class="pr-12">{{ props.comment }}</div>
+            <br />
+            <div class="flex flex-row pr-4">
+                <Icon color="rgb(96, 165, 250)" size="30">
+                    <ThumbUpOffAltRound></ThumbUpOffAltRound>
+                </Icon>
+
+                <div class="flex flex-wrap pr-2">
+                    <div v-for="item in props.tag" :key="item">
+                        <div
+                            class="border-2 p-1 mx-2 mb-1 text-sm truncate hover:text-clip"
+                        >
+                            {{ item }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
