ca15a88c013c8f81abc0c65dae37250ad6c43e13
GeniusCorn <hi@nicecorn.com>
2022-04-07 13:49:41 +0800

    feat: 商家信息页商家信息组件

---
+++ src/components/merchant/MerchantInformation.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="p-6">
+        <div>商家信息</div>
+        <br />
+        
+        <div class="item">该商家支持发票，请下单写好发票抬头</div>
+
+        <div class="item">品类：其他菜系</div>
+
+        <div class="item">城里粥坊</div>
+
+        <div class="item">营业时间 10:30 ~ 20:30</div>
+    </div>
+</template>
+
+<style scoped>
+.item {
+    @apply border-b-2 mb-4;
+}
+</style>
