0015708ee166169c36175898c8d00ca3ea7ec4bc
GeniusCorn <hi@nicecorn.com>
2022-04-09 14:21:21 +0800

    feat: 导航栏活动菜单样式

--- src/components/common/NavigateBar.vue
+++ src/components/common/NavigateBar.vue
@@ -1,15 +1,46 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computed, reactive, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const isActive = reactive({
+    commodity: false,
+    comment: false,
+    merchant: false,
+});
+watch(route, () => {
+    const url = computed(() => route.meta.url);
+    if (url.value === '/') {
+        isActive.commodity = true;
+        isActive.comment = false;
+        isActive.merchant = false;
+    } else if (url.value === '/comments') {
+        isActive.commodity = false;
+        isActive.comment = true;
+        isActive.merchant = false;
+    } else {
+        isActive.commodity = false;
+        isActive.comment = false;
+        isActive.merchant = true;
+    }
+});
+</script>
 
 <template>
     <div class="grid grid-cols-3 justify-items-center my-2 h-fit">
         <router-link to="/">
-            <div class="item">商品</div>
+            <div :class="{ isActive: isActive.commodity }" class="item">
+                商品
+            </div>
         </router-link>
         <router-link to="/comments">
-            <div class="item">评论</div>
+            <div :class="{ isActive: isActive.comment }" class="item">评论</div>
         </router-link>
         <router-link to="/merchant">
-            <div class="item">商家</div>
+            <div :class="{ isActive: isActive.merchant }" class="item">
+                商家
+            </div>
         </router-link>
     </div>
 </template>
@@ -17,5 +48,9 @@
 <style scoped>
 .item {
     @apply text-lg text-center;
+}
+
+.isActive {
+    @apply underline decoration-solid decoration-blue-400 underline-offset-4;
 }
 </style>
