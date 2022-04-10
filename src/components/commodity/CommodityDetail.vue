7930fbdf4f8648d1aef276e816c11f3c28e6c2e5
GeniusCorn <hi@nicecorn.com>
2022-04-10 11:07:08 +0800

    feat: 商品页面商品详情过渡动画

--- src/components/commodity/CommodityDetail.vue
+++ src/components/commodity/CommodityDetail.vue
@@ -41,54 +41,64 @@ watch(show, async () => {
 </script>
 
 <template>
-    <div v-show="props.show" class="absolute inset-0 bg-slate-200">
-        <div ref="detailRef" class="overflow-hidden h-full">
-            <div>
-                <div class="absolute left-0 top-0" @click="$emit('close')">
-                    <Icon color="rgb(96, 165, 250)" size="58">
-                        <ArrowCircleLeftRound></ArrowCircleLeftRound>
-                    </Icon>
-                </div>
-
-                <img
-                    :alt="commodity[name].title"
-                    :src="commodity[name].img"
-                    class="h-96 w-screen object-cover"
-                />
-                <div class="relative bg-white p-6">
-                    <div class="font-semibold">
-                        {{ commodity[name].title }}
-                    </div>
-                    <div class="text-slate-400">
-                        <span class="mr-2"
-                            >月售 {{ commodity[name].monthly_sale }}</span
-                        >
-                        <span>好评 {{ commodity[name].praise }}</span>
+    <Transition
+        enter-active-class="animate__animated animate__slideInRight"
+        leave-active-class="animate__animated animate__slideOutRight"
+        name="custom-classes"
+    >
+        <div v-show="props.show" class="absolute inset-0 bg-slate-200">
+            <div ref="detailRef" class="overflow-hidden h-full">
+                <div>
+                    <div class="absolute left-0 top-0" @click="$emit('close')">
+                        <Icon color="rgb(96, 165, 250)" size="58">
+                            <ArrowCircleLeftRound></ArrowCircleLeftRound>
+                        </Icon>
                     </div>
 
-                    <div class="text-red-400">
-                        ￥ {{ commodity[name].price }}
-                    </div>
-                    <div class="absolute bottom-6 right-8">
-                        <CountItem
-                            :price="props.price"
-                            :title="props.title"
-                        ></CountItem>
-                    </div>
-                </div>
-                <br />
+                    <img
+                        :alt="commodity[name].title"
+                        :src="commodity[name].img"
+                        class="h-96 w-screen object-cover"
+                    />
+                    <div class="relative bg-white p-6">
+                        <div class="font-semibold">
+                            {{ commodity[name].title }}
+                        </div>
+                        <div class="text-slate-400">
+                            <span class="mr-2"
+                                >月售 {{ commodity[name].monthly_sale }}</span
+                            >
+                            <span>好评 {{ commodity[name].praise }}</span>
+                        </div>
 
-                <div class="p-6 bg-white">
-                    <div class="font-semibold">商品信息</div>
-                    <div>{{ commodity[name].description }}</div>
-                </div>
-                <br />
+                        <div class="text-red-400">
+                            ￥ {{ commodity[name].price }}
+                        </div>
+                        <div class="absolute bottom-6 right-8">
+                            <CountItem
+                                :price="props.price"
+                                :title="props.title"
+                            ></CountItem>
+                        </div>
+                    </div>
+                    <br />
 
-                <div class="p-6 bg-white">
-                    <div class="font-semibold">商品评价</div>
-                    <CommentList :title="props.title"></CommentList>
+                    <div class="p-6 bg-white">
+                        <div class="font-semibold">商品信息</div>
+                        <div>{{ commodity[name].description }}</div>
+                    </div>
+                    <br />
+
+                    <div class="p-6 bg-white">
+                        <div class="font-semibold">商品评价</div>
+                        <CommentList :title="props.title"></CommentList>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </Transition>
 </template>
+
+<style scoped>
+@import 'https://cdn.staticfile.org/animate.css/4.1.1/animate.min.css';
+</style>
