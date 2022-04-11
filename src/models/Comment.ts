4e4875b45ecadcf7010ff3afe682f2f348918f64
GeniusCorn <hi@nicecorn.com>
2022-04-11 16:33:25 +0800

    test: 增加部分模拟数据

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -13,7 +13,7 @@ export const zhangwuji: Comment = {
     userName: '张无忌',
     rating: 5,
     comment: '不错，粥很好喝，我经常吃这一家，非常赞，以后也会常来吃，强烈推荐',
-    tag: ['八宝酱菜'],
+    tag: ['娃娃菜炖豆腐', '100% NFC 橙汁'],
     time: '2022-04-01 07:34',
 };
 
@@ -22,7 +22,7 @@ export const yangguo: Comment = {
     userName: '杨过',
     rating: 3.8,
     comment: '',
-    tag: ['100% NFC 芒果混合汁', '皮蛋瘦肉粥', '扁豆焖面'],
+    tag: ['100% NFC 芒果混合汁', '皮蛋瘦肉粥', '手撕包菜'],
     time: '2022-04-02 22:14',
 };
 
@@ -31,7 +31,7 @@ export const zhoubotong: Comment = {
     userName: '周伯通',
     rating: 4,
     comment: '服务态度不错',
-    tag: ['南瓜粥', '皮蛋瘦肉粥', '扁豆焖面'],
+    tag: ['拍黄瓜', '香酥黄金鱼', '娃娃菜炖豆腐'],
     time: '2022-04-03 11:56',
 };
 
@@ -40,11 +40,51 @@ export const weixiaobao: Comment = {
     userName: '韦小宝',
     rating: 2,
     comment: '派送时间太长',
-    tag: ['南瓜粥', '皮蛋瘦肉粥', '扁豆焖面'],
+    tag: ['100% NFC 芒果混合汁', '八宝酱菜', '娃娃菜炖豆腐'],
     time: '2022-04-04 07:24',
 };
 
+export const liangzijin: Comment = {
+    userAvatar: '梁子进',
+    userName: '梁子进',
+    rating: 4.2,
+    comment: '服务态度不错',
+    tag: ['皮蛋瘦肉粥', '红豆粥', '100% NFC 橙汁'],
+    time: '2022-04-05 11:09',
+};
+
+export const duguqiubai: Comment = {
+    userAvatar: '独孤求败',
+    userName: '独孤求败',
+    rating: 4.9,
+    comment: '良心店铺，推荐大家来试试看',
+    tag: ['牛肉馅饼', '葱花饼', '拍黄瓜'],
+    time: '2022-04-05 14:59',
+};
+
+export const mujianping: Comment = {
+    userAvatar: '沐剑屏',
+    userName: '沐剑屏',
+    rating: 1.2,
+    comment: '送货的速度跟蜗牛一样，太慢了',
+    tag: ['田园蔬菜粥', '香酥黄金鱼'],
+    time: '2022-04-06 19:19',
+};
+
+export const murongfu: Comment = {
+    userAvatar: '慕容复',
+    userName: '慕容复',
+    rating: 4.7,
+    comment: '粥的味道还不错',
+    tag: ['田园蔬菜粥'],
+    time: '2022-04-06 20:11',
+};
+
 export const commentList: Comment[] = [
+    murongfu,
+    mujianping,
+    duguqiubai,
+    liangzijin,
     weixiaobao,
     zhoubotong,
     yangguo,
--- src/models/Commodity.ts
+++ src/models/Commodity.ts
@@ -5,6 +5,13 @@ import img4 from '../assets/images/6557e9564c133f17f1227c93bb4f9be6.jpeg';
 import img5 from '../assets/images/48265c173c4b4c3283b62b7ff0c530a5.jpg';
 import img6 from '../assets/images/8d97c3d36cc236510e1efcbcb60ae35c.jpeg';
 import img7 from '../assets/images/f36b0234d4742fd6555578e32dc5c71d.jpeg';
+import img8 from '../assets/images/src=http___5b0988e595225.cdn.sohucs.com_images_20181201_37f4a91c26f646318ee298eeff181088.jpeg&refer=http___5b0988e595225.cdn.sohucs.webp';
+import img9 from '../assets/images/u=2142987921,2724248309&fm=253&fmt=auto&app=138&f=PNG.webp';
+import img10 from '../assets/images/u=2433146923,3078885835&fm=253&fmt=auto&app=138&f=JPEG.webp';
+import img11 from '../assets/images/src=http___img.mp.itc.cn_upload_20170628_6733ae7eec70431bb974753116d79188_th.jpg&refer=http___img.mp.itc.webp';
+import img12 from '../assets/images/src=http___recipe0.hoto.cn_pic_step_l_fa_b4_5420282.jpg&refer=http___recipe0.hoto.webp';
+import img13 from '../assets/images/src=http___bkimg.cdn.bcebos.com_pic_f11f3a292df5e0feaf0de878526034a85edf72b5&refer=http___bkimg.cdn.bcebos.webp';
+import img14 from '../assets/images/src=http___bkimg.cdn.bcebos.com_pic_c83d70cf3bc79f3df8dcc3b72ceada11728b461083ab&refer=http___bkimg.cdn.bcebos.webp';
 
 export interface Commodity {
     name: string;
@@ -14,7 +21,9 @@ export interface Commodity {
     praise: number;
     price: number;
     img: string;
-    description: string;
+    description?: string;
+
+    [index: string]: any;
 }
 
 // * 模拟商家商品数据
@@ -95,6 +104,83 @@ export const mangoMix: Commodity = {
     description: '我是 100% NFC 芒果混合汁',
 };
 
+export const lentilCasserole: Commodity = {
+    name: 'lentilCasserole',
+    title: '扁豆焖面',
+    tag: '小吃主食',
+    monthly_sale: 288,
+    praise: 96,
+    price: 14,
+    img: img8,
+    description: '我是扁豆焖面',
+};
+
+export const scallionCake: Commodity = {
+    name: 'scallionCake',
+    title: '葱花饼',
+    tag: '小吃主食',
+    monthly_sale: 124,
+    praise: 85,
+    price: 10,
+    img: img9,
+    description: '我是葱花饼',
+};
+
+export const beefPie: Commodity = {
+    name: 'beefPie',
+    title: '牛肉馅饼',
+    tag: '小吃主食',
+    monthly_sale: 114,
+    praise: 96,
+    price: 12,
+    img: img10,
+    description: '我是牛肉馅饼',
+};
+
+export const skinnyEggAndLeanPorkPorridge: Commodity = {
+    name: 'skinnyEggAndLeanPorkPorridge',
+    title: '皮蛋瘦肉粥',
+    tag: '特色粥品',
+    monthly_sale: 331,
+    praise: 98,
+    price: 10,
+    img: img11,
+    description: '我是皮蛋瘦肉粥',
+};
+
+export const redBeanPorridge: Commodity = {
+    name: 'redBeanPorridge',
+    title: '红豆粥',
+    tag: '特色粥品',
+    monthly_sale: 45,
+    praise: 95,
+    price: 12,
+    img: img12,
+    description: '我是红豆粥',
+};
+
+export const pumpkinPorridge: Commodity = {
+    name: 'pumpkinPorridge',
+    title: '南瓜粥',
+    tag: '特色粥品',
+    monthly_sale: 93,
+    praise: 91,
+    price: 9,
+    img: img13,
+    description: '我是南瓜粥',
+};
+
+export const gardenVegetablePorridge: Commodity = {
+    name: 'gardenVegetablePorridge',
+    title: '田园蔬菜粥',
+    tag: '特色粥品',
+    monthly_sale: 23,
+    praise: 96,
+    price: 10,
+    img: img14,
+    description: '我是田园蔬菜粥',
+};
+
 export const commodityItemList: Commodity[] = [
     // 精选热菜
     stewedTofuWithBabyVegetables,
@@ -106,4 +192,13 @@ export const commodityItemList: Commodity[] = [
     // 果品果汁
     orangeJuice,
     mangoMix,
+    // 小吃主食
+    lentilCasserole,
+    scallionCake,
+    beefPie,
+    // 特色粥品
+    skinnyEggAndLeanPorkPorridge,
+    redBeanPorridge,
+    pumpkinPorridge,
+    gardenVegetablePorridge,
 ];
